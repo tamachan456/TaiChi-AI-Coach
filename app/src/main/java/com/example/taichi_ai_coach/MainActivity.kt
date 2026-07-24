1ff0beca7494f680d22186d2031aeb353b1c4756
tamachan456 <tamachan456ipod@gmail.com>
2026-07-24 16:04:24 +0900

    Initial Flutter Project

---
+++ app/src/main/java/com/example/taichi_ai_coach/MainActivity.kt
@@ -0,0 +1,49 @@
+
+
+package com.example.taichi_ai_coach
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.taichi_ai_coach.ui.theme.TaiChi_AI_CoachTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TaiChi_AI_CoachTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TaiChi_AI_CoachTheme {
+        Greeting("Android")
+    }
+}
---
+++ taichi_ai_coach/android/app/src/main/kotlin/com/example/taichi_ai_coach/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.taichi_ai_coach
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
